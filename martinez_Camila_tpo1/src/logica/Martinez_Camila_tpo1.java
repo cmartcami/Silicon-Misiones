fac57e2dde084b8bedbc34faa4935e71c9983263
Camila Martinez <86632005+cmartcami@users.noreply.github.com>
2021-10-08 08:44:10 -0300

    Add files via upload

---
+++ martinez_Camila_tpo1/src/logica/Martinez_Camila_tpo1.java
@@ -0,0 +1,15 @@
+
+package logica;
+
+import igu.Pantalla;
+
+public class Martinez_Camila_tpo1 {
+
+    public static void main(String[] args) {
+        
+        Pantalla panta = new Pantalla();
+        panta.setVisible(true);
+        panta.setLocationRelativeTo(null);
+    }
+    
+}
